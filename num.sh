979b81f47e86b11e3992ce9e7b006568ad42f951
Alex Guo <alextooter@gmail.com>
2017-09-03 09:24:11 +0800

    add delay

--- num.sh
+++ num.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 for ((i=0;i <200;i++))
 do
-    say 1,
-    sleep 1
-    say 2
-    sleep 1
+    say 1, 
+    sleep 0.4
+    say 2, 
+    sleep 0.4
+    say 3, 
+    sleep 0.4
+    say 4, 
+    sleep 0.4
+    say 5, 
+    sleep 0.4
+    say 6, 
+    sleep 0.4
+    say 7, 
+    sleep 0.4
+    say 8, 
+    sleep 0.4
+    say 9, 
+    sleep 0.4
+    say 10, 
+    sleep 0.4
 done
